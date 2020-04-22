a3f23283debb4796a0d0ee3c04d4f52a71205f57
root <root@803d996bac86>
2020-04-22 22:00:29 +0000

    Lottery and Magic 8s.

---
+++ src/_06_lottery_numbers/Lotterynumbers.java
@@ -0,0 +1,36 @@
+package _06_lottery_numbers;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Lotterynumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Zyklon =new Random().nextInt(11);
+		int Pinfari =new Random().nextInt(11);
+		int Arrow =new Random().nextInt(11);
+		int Zierer =new Random().nextInt(11);
+		int Dinn =new Random().nextInt(11);
+		int Loof =new Random().nextInt(11);
+		String Mack="Here's your numbers !"+ Zyklon + Pinfari + Arrow + Zierer + Dinn + Loof;
+		JOptionPane.showMessageDialog(null, );
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
